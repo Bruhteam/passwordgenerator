b5636b77a437e19d4f0a483396760925032421b7
Galuxosi <jker1786@gmail.com>
2022-09-27 21:42:33 +0300

    Version 1.2.0
    Bug fixes. Added "Show Password" CheckButton.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Імпорт бібліотек | Libraries importing
+from distutils.cmd import Command
+from msilib.schema import CheckBox
 from pydoc import stripid
 import random
 import string
 from tkinter import *
 from tkinter import ttk
-
-from pyautogui import sleep
 
 # Інформація про вікно / Windows' info
 tkWindow = Tk()  
@@ -32,30 +32,45 @@ def passgen():
     random.shuffle(password)
 
     # Вивід паролю / Password output
-    print("".join(password))
     passinput()
-    switch()
 
-def switch():
-    if GenButton["state"] == "NORMAL":
-        GenButton["state"] = "DISABLED"
+# Команда для зміни стану кнопки / Change button state
+def changeState():
+    if (GenButton['state'] == NORMAL):
+        GenButton['state'] = DISABLED
+    else:
+        GenButton['state'] = NORMAL
 
+# Вивід пароля в ячейку / Password output to Entry Widget
 def passinput():
-    passEntry.insert(0, "")
     passEntry.insert(0, "".join(password))
+    changeState()
+
+# Команда для показування пароля / Show password command
+def ShowPass():
+    if (passEntry['show'] == "*"):
+        passEntry['show'] = ""
+    else:
+        passEntry["show"] = "*"
+
 # Ввести довжину пароля / Enter Password length
 lengthLabel = Label(tkWindow, text="Password length:").grid(row=0, column=0)
 length = IntVar()
 lengthEntry = Entry(tkWindow, textvariable=length).grid(row=0, column=1)
 
-passEntry = Entry(tkWindow)
+# Вивід паролю / Password output
+passEntry = Entry(tkWindow, show="*")
 passEntry.grid(row=1, column=1)
 
 # Генерація пароля / Password Generation Button
 GenButton = Button(tkWindow, text="Generate", command=passgen, state=NORMAL)
 GenButton.grid(row=1, column=0) 
+
+# Показати пароль / Show Password Checkbox
+ShowPassBox = Checkbutton(tkWindow, text="Show Password", command=ShowPass).grid(row=2, column=1)
+
 # Інформація про версію / Version Info
-versionInfo = Label(tkWindow, text="Version 1.0.1").place(x=178, y=110)
+versionInfo = Label(tkWindow, text="Version 1.2.0").place(x=178, y=110)
 versionInfo = Label(tkWindow, text="Bruhteam - Makepass").place(x=130, y=130)
 
 tkWindow.mainloop()
